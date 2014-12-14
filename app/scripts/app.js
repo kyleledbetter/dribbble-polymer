b6f9f3cbf9ab83bfbde089a4e85c482ed1de21f8
kyleledbetter <kyle@kyleledbetter.com>
2014-12-13 18:20:03 -0600

    Customizing for dribbble app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,21 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+    // custom transformation: scale header's title
+    var titleStyle = document.querySelector('.title').style;
+    addEventListener('core-header-transform', function(e) {
+      var d = e.detail;
+      var m = d.height - d.condensedHeight;
+      var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
+      titleStyle.transform = titleStyle.webkitTransform =
+          'scale(' + scale + ') translateZ(0)';
+    });
+    //tabs
+    var pages = document.querySelector('core-pages');
+    var tabs = document.querySelector('[drawer] paper-tabs');
+    tabs.addEventListener('core-select', function() {
+      pages.selected = tabs.selected;
+    });
   });
 
 // wrap document so it plays nice with other libraries
